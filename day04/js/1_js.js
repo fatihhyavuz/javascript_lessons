66820e4ffde317ff73ebed511f9ff19f8887e778
fatih yavuz <fatihyvz993@gmail.com>
2023-12-13 10:10:09 +0300

    v1

---
+++ day04/js/1_js.js
@@ -0,0 +1,12 @@
+// queryselector eslesen ilk elemani secer
+const headel = document.querySelector("h1");
+console.log(headel)
+
+const aboutusel = document.querySelector("#aboutus")
+console.log(aboutusel)
+
+const spanel = document.querySelector(("#aboutus span"))
+
+console.log(spanel)
+
+//
