e94149fa8b0b1bc80f66d5989610981d0caed152
fatih yavuz <fatihyvz993@gmail.com>
2023-12-14 08:33:23 +0300

    v1

--- day04/js/1_js.js
+++ day04/js/1_js.js
@@ -5,8 +5,21 @@ console.log(headel)
 const aboutusel = document.querySelector("#aboutus")
 console.log(aboutusel)
 
-const spanel = document.querySelector(("#aboutus span"))
+const spanel = document.querySelector("#aboutus span")
 
 console.log(spanel)
 
-//
+const menuel = document.querySelector("ul.menu")
+console.log(menuel)
+
+const menuitemel = document.querySelector("ul.menu li:nth-child(3)")
+console.log(menuitemel)
+
+headel.innerText="hello javascript."
+
+//queryselectorall eslesen tum elemanlari getirir
+
+const spanels = document.querySelectorAll("aboutus>span")
+console.log(spanels)
+
+spanels[0].innerText="hello"
